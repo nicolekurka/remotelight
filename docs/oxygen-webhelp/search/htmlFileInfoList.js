938c5ebf12e335d99d27ed598d17778e222d8d7c
nicolekurka <nkurka97@vt.edu>
2017-03-28 10:28:36 -0400

    so like here's that moving the out folder and stuff

---
+++ docs/oxygen-webhelp/search/htmlFileInfoList.js
@@ -0,0 +1,14 @@
+fil = new Array();
+fil["0"]= "how-to-program-bulbs-to-groups.html@@@Programming Light Bulbs to Lighting Groups@@@You can program one or more light bulbs to a lighting group to operate that group with your remote control...";
+fil["1"]= "how-to-turn-on-off-dim-lights.html@@@Turning On/Off or Dimming a Lights@@@You can turn on, turn off, or dim a lighting group by pressing the group number button and then pressing the power button or the dimmer control...";
+fil["2"]= "intro-considerations-planning-light-network.html@@@Considerations for Planning Your Lighting Network@@@Before installing your network lighting, you should plan how you want your lighting network to behave...";
+fil["3"]= "intro-low-power-networking.html@@@Low-Power Networking@@@Network lighting operates at a low level of networking power but can successfully connect at long distances because they can send information from light bulb to light bulb...";
+fil["4"]= "intro-max-number-bulbs.html@@@Maximum Number of Network Light Bulbs@@@Testing shows you should network no more than 250 light bulbs to maintain reliable performance...";
+fil["5"]= "intro-network-lighting.html@@@Basic Concepts of Network Lighting@@@You can network LED light bulbs together to operate wirelessly from a remote control...";
+fil["6"]= "intro-product.html@@@Remote Lighting Overview@@@Remote lighting allows you to operate network-based home lighting through a remote control...";
+fil["7"]= "legal-disc.html@@@Legal Disclaimer@@@People can mess up...";
+fil["8"]= "md topic contact.html@@@Contact Us@@@If you have any concerns, please contact us through our email . If you think we\u2019re doing something right, send us an email too...";
+fil["9"]= "ref-remote-control-components.html@@@Remote Control Components@@@The remote control has components that allow you to program and operate the light bulbs on your lighting network...";
+fil["10"]= "ref-specifications-led-bulb.html@@@LED Light Bulb Specifications@@@Specifications describing the electronics and technology inside the LED light bulb...";
+fil["11"]= "ref-specifications-overview.html@@@Product Specifications@@@The network lighting components have specific electronic and technical specifications...";
+fil["12"]= "ref-specifications-remote-control.html@@@Remote Control Specifications@@@Specifications describing the electronics and technology inside the remote control...";
---
+++ docs/oxygen-webhelp/search/htmlFileList.js
@@ -0,0 +1,16 @@
+//List of files which are indexed.
+fl = new Array();
+fl["0"]= "how-to-program-bulbs-to-groups.html";
+fl["1"]= "how-to-turn-on-off-dim-lights.html";
+fl["2"]= "intro-considerations-planning-light-network.html";
+fl["3"]= "intro-low-power-networking.html";
+fl["4"]= "intro-max-number-bulbs.html";
+fl["5"]= "intro-network-lighting.html";
+fl["6"]= "intro-product.html";
+fl["7"]= "legal-disc.html";
+fl["8"]= "md topic contact.html";
+fl["9"]= "ref-remote-control-components.html";
+fl["10"]= "ref-specifications-led-bulb.html";
+fl["11"]= "ref-specifications-overview.html";
+fl["12"]= "ref-specifications-remote-control.html";
+var doStem = false;searchLoaded = true;
